5967365c9a66ca0e007cbd0beadc59e94d9d867e
Jimmy Berglund <jimmy_berglund@hotmail.com>
2021-04-20 10:42:56 +0200

    Added data to be shown
    The country, state, active, active cases and the last time it was
    updated are now shown on the web in addition to the previous data.
    I also further deleted some of the data files, when we actually start
    plotting the data we will introduce some granularity that makes it so we
    can handle such large amounts of data, but for now I will use a smaller
    dataset.

--- application/api.py
+++ application/api.py
@@ -8,7 +8,11 @@ class API:
         tmp = []
         for i in range(len(self.data['cases'])):
             case = {}
+            case['country'] = self.data['cases'][i].getCountryRegion()
+            case['province'] = self.data['cases'][i].getProvinceState()
+            case['update'] = self.data['cases'][i].getLastUpdate()
             case['confirmed'] = self.data['cases'][i].getConfirmed()
+            case['active'] = self.data['cases'][i].getActive()
             case['deaths'] = self.data['cases'][i].getDeaths()
             case['recovered'] = self.data['cases'][i].getRecovered()
             tmp.append(case)
