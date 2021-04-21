a98fec521e1e622145156121d9004827649f780c
kynkel <markus@kunkel.se>
2021-04-21 14:04:37 +0200

    improved performance
    commented out code not needed right now

--- application/case.py
+++ application/case.py
@@ -2,8 +2,8 @@ class Case:
 
     def __init__(self):
         self.form = None
-        self.fips = None
-        self.admin2 = None
+        #self.fips = None
+        #self.admin2 = None
         self.provinceState = None
         self.countryRegion = None
         self.lastUpdate = None
@@ -13,18 +13,18 @@ class Case:
         self.deaths = None
         self.recovered = None
         self.active = None
-        self.combinedKey = None
-        self.incidentRate = None
-        self.caseFatalityRatio = None
-        self.IncidenceRate = None
+        #elf.combinedKey = None
+        #elf.incidentRate = None
+        #elf.caseFatalityRatio = None
+        #elf.IncidenceRate = None
 
 
     def getForm(self):
         return self.form
-    def getFips(self):
-        return self.fips
-    def getAdmin2(self):
-        return self.admin2
+    #def getFips(self):
+    #    return self.fips
+    #def getAdmin2(self):
+    #    return self.admin2
     def getProvinceState(self):
         return self.provinceState
     def getCountryRegion(self):
@@ -43,21 +43,21 @@ class Case:
         return self.recovered
     def getActive(self):
         return self.active
-    def getCombinedKey(self):
-        return self.combinedKey
-    def getIncidentRate(self):
-        return self.incidentRate
-    def getCaseFatalityRatio(self):
-        return self.caseFatalityRatio
-    def getIncidenceRate(self):
-        return self.caseIncidenceRate
+    #def getCombinedKey(self):
+    #    return self.combinedKey
+    #def getIncidentRate(self):
+    #    return self.incidentRate
+    #def getCaseFatalityRatio(self):
+    #    return self.caseFatalityRatio
+    #def getIncidenceRate(self):
+    #    return self.caseIncidenceRate
 
     def setForm(self,form):
         self.form=form
-    def setFips(self,fips):
-        self.fips=fips
-    def setAdmin2(self,admin2):
-        self.admin2=admin2
+    #def setFips(self,fips):
+    #    self.fips=fips
+    #def setAdmin2(self,admin2):
+    #    self.admin2=admin2
     def setProvinceState(self,provinceState):
         self.provinceState=provinceState
     def setCountryRegion(self,countryRegion):
@@ -76,11 +76,11 @@ class Case:
         self.recovered=recovered
     def setActive(self,active):
         self.active=active
-    def setCombinedKey(self,combinedKey):
-        self.combinedKey=combinedKey
-    def setIncidentRate(self,incidentRate):
-        self.incidentRate=incidentRate
-    def setCaseFatalityRatio(self,caseFatalityRatio):
-        self.caseFatalityRatio=caseFatalityRatio
-    def setIncidenceRate(self,IncidenceRate):
-        self.IncidenceRate=IncidenceRate
+    #def setCombinedKey(self,combinedKey):
+    #    self.combinedKey=combinedKey
+    #def setIncidentRate(self,incidentRate):
+    #    self.incidentRate=incidentRate
+    #def setCaseFatalityRatio(self,caseFatalityRatio):
+    #    self.caseFatalityRatio=caseFatalityRatio
+    #def setIncidenceRate(self,IncidenceRate):
+    #    self.IncidenceRate=IncidenceRate
